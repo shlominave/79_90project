315ac5682390203ef093fdbfe9198150f64c9610
shlominave <s1170@motzkine.ort.org.il>
2020-11-16 15:24:45 +0200

    commit

--- app/src/main/java/com/example/a79_90project/MainActivity.java
+++ app/src/main/java/com/example/a79_90project/MainActivity.java
@@ -13,17 +13,17 @@ public class MainActivity extends Activity {
     private double currentBillTotal;
     private int Currentcustomprecent;
     private EditText billET;
-    private EditText tip10ET;
-    private EditText total0ET;
-    private EditText tip15ET;
-    private EditText total15ET;
-    private EditText total20ET;
-    private EditText tip20ET;
+
     private EditText tipcustomET;
     private TextView customtipTV;
     private EditText totalCustomET;
     private SeekBar customSeekbar;
-
+    private EditText Tip10ET ;
+    private EditText Tip15ET;
+    private EditText Tip20ET;
+    private EditText Total0ET;
+    private EditText Total15ET;
+    private EditText Total20ET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +32,34 @@ public class MainActivity extends Activity {
         customtipTV = findViewById(R.id.CustomTipTextView);
         tipcustomET = findViewById(R.id.tipcustomEditText);
         totalCustomET = findViewById(R.id.totalcustomET);
+        Tip10ET=findViewById(R.id.tip10ET);
+        Tip15ET=findViewById(R.id.tip15ET);
+        Tip20ET=findViewById(R.id.tip20ET);
+        Total0ET=findViewById(R.id.total10ET);
+        Total15ET=findViewById(R.id.total15ET);
+        Total20ET=findViewById(R.id.total20ET);
         customSeekbar = findViewById(R.id.CustomseekBar);
         customSeekbar.setOnSeekBarChangeListener(customSeekBarListener);
         billET=findViewById(R.id.billET);
-
-
         billET.addTextChangedListener(BillETwatcher);
 
     }
+    private void UpdateStandard()
+    {
+        double tenprecentTip=currentBillTotal*0.1;
+        double tenprecentTotal=currentBillTotal+tenprecentTip;
+        Tip10ET.setText(String.format("%.02f",tenprecentTip));
+        Total0ET.setText(String.format("%.02f",tenprecentTotal));
+        double fifteenprecentTip=currentBillTotal*0.15;
+        double fifteenprecentTotal=currentBillTotal+fifteenprecentTip;
+        Tip15ET.setText(String.format("%.02f",fifteenprecentTip));
+        Total15ET.setText(String.format("%.02f",fifteenprecentTotal));
+        double twentyprecentTip=currentBillTotal*0.2;
+        double twentyprecentTotal=currentBillTotal+twentyprecentTip;
+        Tip20ET.setText(String.format("%.02f",twentyprecentTip));
+        Total20ET.setText(String.format("%.02f",twentyprecentTotal));
+        }
+
     private void UpdateCustom() {
         customtipTV.setText(Currentcustomprecent + "%");
         double customTipAmount = currentBillTotal * Currentcustomprecent * 0.01;
@@ -59,9 +79,10 @@ public class MainActivity extends Activity {
                currentBillTotal = Double.parseDouble(s.toString());
            } catch (NumberFormatException E) {
                currentBillTotal = 0.0;
-               UpdateCustom();
-           }
 
+           }
+           UpdateStandard();
+           UpdateCustom();
        }
        @Override
        public void afterTextChanged(Editable s) {
